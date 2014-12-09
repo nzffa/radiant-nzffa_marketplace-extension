08819fce1db15aa17f47e28fc163d7cc212fec03
Benny Degezelle <hi@monkeypatch.be>
2014-12-09 20:01:31 +0100

    Remove remnants of Person as authentication model as it was never actually used

--- config/routes.rb
+++ config/routes.rb
@@ -62,9 +62,5 @@ ActionController::Routing::Routes.draw do |map|
     end
   end
 
-  map.show_login_area "show_login_area", :controller => "person_sessions", :action => "show_login_area"
-  map.person_login "person_login", :controller => "person_sessions", :action => "new"
-  map.person_logout "person_logout", :controller => "person_sessions", :action => "destroy"
-  map.resources :person_sessions
   map.new_reader "/become-a-member", :controller => 'membership', :action => 'register'
 end
