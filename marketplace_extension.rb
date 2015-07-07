21822101bff62c1fbdd37d146b50c064c0dd9aca
Benny Degezelle <hi@monkeypatch.be>
2015-07-07 23:57:26 +0200

    Move everything that is not marketplace into new nzffa extension

--- marketplace_extension.rb
+++ marketplace_extension.rb
@@ -1,8 +1,6 @@
 # Uncomment this if you reference any of your controllers in activate
 # require_dependency "application_controller"
 require "radiant-marketplace-extension"
-require 'reader_mixin'
-#require 'reader_validator'
 
 class MarketplaceExtension < Radiant::Extension
   version     RadiantMarketplaceExtension::VERSION
@@ -17,18 +15,7 @@ class MarketplaceExtension < Radiant::Extension
 
   def activate
     # this seem to work as a method to overwrite the readers views
-    Reader.send :include, ReaderMixin
-    AccountsController.send :include, Marketplace::AccountsControllerExtension
-
     Page.class_eval { include Marketplace }
-    tab 'Readers' do
-      add_item "Readers Plus", "/admin/readers_plus"
-      add_item "Subscriptions", "/admin/subscriptions"
-      add_item "Orders", "/admin/orders"
-      add_item "Reports", "/admin/reports"
-      add_item "Print Subscriptions", "/admin/subscriptions/batches_to_print"
-    end
-
     tab 'Marketplace' do
       add_item "Marketplace", "/admin/adverts"
     end
